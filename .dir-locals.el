c6f2a49ffdbdcbd82f7e5361517dcb75676d2fb5
supp-creator <tyronesarmiento3@gmail.com>
2026-06-13 22:09:12 +0800

    Overhaul Org-Roam

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "Multinomial th"))))
